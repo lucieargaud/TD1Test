6ff9b1533d0e3b9e238a47c9245a3c823a7bd066
lucieargaud <lucieargaud@gmail.com>
2023-03-21 15:50:58 +0100

    Produit Rep

--- src/main/java/com/inti/repository/IProduitRepository.java
+++ src/main/java/com/inti/repository/IProduitRepository.java
@@ -1,11 +1,11 @@
-package com.inti.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.inti.model.Produit;
-
-@Repository
-public interface IProduitRepository extends JpaRepository<Produit, Integer> {
-
-}
+//package com.inti.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.inti.model.Produit;
+//
+//@Repository
+//public interface IProduitRepository extends JpaRepository<Produit, Integer> {
+//
+//}

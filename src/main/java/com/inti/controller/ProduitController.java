56554917edc2c93d055dd8800af938ec0e6126de
lucieargaud <lucieargaud@gmail.com>
2023-03-21 15:52:16 +0100

    Produit Rep Con

--- src/main/java/com/inti/controller/ProduitController.java
+++ src/main/java/com/inti/controller/ProduitController.java
@@ -1,27 +1,27 @@
-package com.inti.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.inti.repository.IProduitRepository;
-
-@Controller
-public class ProduitController {
-
-	@Autowired
-	IProduitRepository ipr;
-	
-//	@GetMapping("hello")
-//	public String hello()
-//	{
-//		return "hello";
+//package com.inti.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//import com.inti.repository.IProduitRepository;
+//
+//@Controller
+//public class ProduitController {
+//
+//	@Autowired
+//	IProduitRepository ipr;
+//	
+////	@GetMapping("hello")
+////	public String hello()
+////	{
+////		return "hello";
+////	}
+//	
+//	@GetMapping("listeProduit")
+//	public String listeSalarie(Model m) {
+//		m.addAttribute("listeProduit", ipr.findAll());
+//		return "listeProduit";
 //	}
-	
-	@GetMapping("listeProduit")
-	public String listeSalarie(Model m) {
-		m.addAttribute("listeProduit", ipr.findAll());
-		return "listeProduit";
-	}
-}
+//}
